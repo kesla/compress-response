74bbed58837fae49663f058185b00c6a72267621
David Björklund <david.bjorklund@gmail.com>
2014-11-16 02:49:34 +0100

    update example

--- example.js
+++ example.js
@@ -1,30 +1,23 @@
 var http = require('http')
 
-  , request = require('request')
-
   , compress = require('./compress-response')
 
   , server = http.createServer(function (req, res) {
-      res.setHeader('content-type', 'application/json')
+      res.setHeader('content-type', 'text/plan')
 
       var stream = compress(req, res)
-      // stream will be gzip-stream if request has correct accept-encoding
-      // and content-type in res is compressible
+      // @stream writes to res
+      // if request had correct accept-encoding & type is compressible
+      //  stream is a compressed stream to res
 
-      stream.write(JSON.stringify({ hello: 'world', foo: { bar: 'bas' } }))
+      stream.write('YEAH!')
       stream.end()
 
     })
 
-server.listen(0, function () {
-  var port = this.address().port
-    , url = 'http://localhost:' + port
-
-  request({ gzip: true, url: url, json: true}, function (err, res, body) {
-    console.log('this is the body')
-    console.log(body)
-    console.log('these are the headers')
-    console.log(res.headers)
-    server.close()
-  })
+require('servertest')(server, '/', { headers: { 'accept-encoding': 'gzip' } }, function (err, res) {
+  console.log('the body is gzip-data')
+  console.log(res.body.toString())
+  console.log('and these are the headers')
+  console.log(res.headers)
 })
