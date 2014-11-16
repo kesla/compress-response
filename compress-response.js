057c3f8afed698353dc666ddf3c694ad091400c1
David Björklund <david.bjorklund@gmail.com>
2014-11-16 01:34:59 +0100

    gzip if accept-encoding think it's ok

--- compress-response.js
+++ compress-response.js
@@ -1,7 +1,21 @@
-var through = require('through2')
+var zlib = require('zlib')
+
+  , bestEncoding = require('best-encoding')
+  , through = require('through2')
 
   , compressResponse = function (req, res) {
-      return res
+      var stream
+
+      if (bestEncoding(req) === 'gzip') {
+        res.setHeader('content-encoding', 'gzip')
+        stream = zlib.createGzip()
+      } else {
+        stream = through()
+      }
+
+      stream.pipe(res)
+
+      return stream
     }
 
 module.exports = compressResponse
--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 var http = require('http')
+  , zlib = require('zlib')
+
   , servertest = require('servertest')
   , test = require('tape')
 
@@ -15,5 +17,26 @@ test('basic', function (t) {
   servertest(server, '/', function (err, res) {
     t.equal(res.body.toString(), 'beepboop')
     t.end(err)
+  })
+})
+
+test('gziped', function (t) {
+  var server = http.createServer(function (req, res) {
+        var stream = compress(req, res)
+        stream.write(JSON.stringify('Hello, world!'))
+        stream.end()
+      })
+    , options = { headers: {'accept-encoding': 'gzip'} }
+
+  servertest(server, '/', options, function (err, res) {
+    if (err) return t.end(err)
+
+    t.equal(res.headers['content-encoding'], 'gzip')
+    zlib.gunzip(res.body, function (err, result) {
+      if (err) return t.end(err)
+
+      t.deepEqual(JSON.parse(result.toString()), 'Hello, world!')
+      t.end()
+    })
   })
 })
